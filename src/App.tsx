20910061860afc48c9fed3fc95053614bde1fd3a
Emmanuel Ajiboye Adeleke <caesar@Emmanuels-MacBook-Pro.local>
2023-09-05 23:30:46 +0100

    added styling for root components

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,7 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      <div></div>
-    </>
-  );
+  return <div className="app"></div>;
 }
 
 export default App;
