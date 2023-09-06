4f6c8c5a915fe95dfcd4342b4638b56e2ec98cc9
Emmanuel Ajiboye Adeleke <caesar@Emmanuels-MacBook-Pro.local>
2023-09-06 22:40:22 +0100

    imported and addedc base routes and browser route globally

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
+import { useEffect } from "react";
 import "./App.css";
+import { BrowserRouter, Route, useLocation, Routes } from "react-router-dom";
 
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+};
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <ScrollToTop />
+        <Routes>{/* <Route path="/" element={<Home />} /> */}</Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
