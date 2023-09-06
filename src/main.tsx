a3a919433dd3dee5c661cecfa4246a6c04e0aafd
Emmanuel Ajiboye Adeleke <caesar@Emmanuels-MacBook-Pro.local>
2023-09-06 15:42:39 +0100

    imported themes and wraapped project

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { ThemeProvider } from "@mui/material/styles";
+import { theme } from "./theme.ts";
+import { CssBaseline } from "@mui/material";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
--- src/theme.ts
+++ src/theme.ts
@@ -52,4 +52,24 @@ export const theme = createTheme({
       light: shades.neutral[100],
     },
   },
+  typography: {
+    fontFamily: ["Fauna One", "sans-serif"].join(","),
+    fontSize: 11,
+    h1: {
+      fontFamily: ["Cinzel", "sans-serif"].join(","),
+      fontSize: 48,
+    },
+    h2: {
+      fontFamily: ["Cinzel", "sans-serif"].join(","),
+      fontSize: 36,
+    },
+    h3: {
+      fontFamily: ["Cinzel", "sans-serif"].join(","),
+      fontSize: 20,
+    },
+    h4: {
+      fontFamily: ["Cinzel", "sans-serif"].join(","),
+      fontSize: 14,
+    },
+  },
 });
