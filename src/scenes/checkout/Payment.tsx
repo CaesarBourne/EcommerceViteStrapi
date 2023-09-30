f3d3a8b679c633042f4dae4b7230d763a3f59140
Emmanuel Ajiboye Adeleke <caesar@Emmanuels-MacBook-Pro.local>
2023-09-30 21:59:29 +0100

    new changes for payment pages

---
+++ src/scenes/checkout/Payment.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Box, Typography } from "@mui/material";
+import TextField from "@mui/material/TextField";
+
+const Payment = () => {
+  return <div>Payment page</div>;
+};
+
+export default Payment;
