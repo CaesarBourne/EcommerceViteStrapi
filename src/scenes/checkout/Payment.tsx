31e64953ebb2c436f77dfefb727bb51354195dca
Emmanuel Ajiboye Adeleke <caesar@Emmanuels-MacBook-Pro.local>
2023-10-01 22:01:02 +0100

    payment type and input changes

--- src/scenes/checkout/Payment.tsx
+++ src/scenes/checkout/Payment.tsx
@@ -1,13 +1,28 @@
 import React from "react";
 import { Box, Typography } from "@mui/material";
 import TextField from "@mui/material/TextField";
+export interface PaymentType {
+    values : any, touched, errors, handleBlur, handleChange }
+}
 
-const Payment = () => {
+const Payment = ({ values, touched, errors, handleBlur, handleChange } : any) => {
   return (
     <Box m="30px 0">
       <Typography sx={{ mb: "15px" }} fontSize="18px">
         Contact Info
       </Typography>
+      <TextField
+        fullWidth
+        type="text"
+        label="Email"
+        onBlur={handleBlur}
+        onChange={handleChange}
+        value={values.email}
+        name="email"
+        error={!!touched.email && !!errors.email}
+        helperText={touched.email && errors.email}
+        sx={{ gridColumn: "span 4", marginBottom: "15px" }}
+      />
     </Box>
   );
 };
