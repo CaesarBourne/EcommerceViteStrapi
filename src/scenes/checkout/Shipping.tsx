08fcf9646706c779a30ec3744b553bc48a8b6a2b
Emmanuel Ajiboye Adeleke <caesar@Emmanuels-MacBook-Pro.local>
2023-10-04 22:52:18 +0100

    shipping more code inputs

--- src/scenes/checkout/Shipping.tsx
+++ src/scenes/checkout/Shipping.tsx
@@ -1,26 +1,51 @@
-
-import React from 'react'
+import React from "react";
 import { Box, Checkbox, FormControlLabel, Typography } from "@mui/material";
 // import AddressForm from "./AddressForm";
 
-const Shipping = () => {
+const Shipping = ({
+  values,
+  touched,
+  errors,
+  handleChange,
+  handleBlur,
+  setFieldValue,
+}: any) => {
   return (
     <Box m="30px auto">
-    {/* BILLING FORM */}
-    <Box>
-      <Typography sx={{ mb: "15px" }} fontSize="18px">
-        Billing Information
-      </Typography>
-      <AddressForm
+      {/* BILLING FORM */}
+      <Box>
+        <Typography sx={{ mb: "15px" }} fontSize="18px">
+          Billing Information
+        </Typography>
+        {/* <AddressForm
         type="billingAddress"
         values={values.billingAddress}
         touched={touched}
         errors={errors}
         handleBlur={handleBlur}
         handleChange={handleChange}
-      />
-    </Box>
-  )
-}
+      /> */}
+      </Box>
 
-export default Shipping
+      <Box mb="20px">
+        <FormControlLabel
+          control={
+            <Checkbox
+              defaultChecked
+              value={values.shippingAddress.isSameAddress}
+              onChange={() =>
+                setFieldValue(
+                  "shippingAddress.isSameAddress",
+                  !values.shippingAddress.isSameAddress
+                )
+              }
+            />
+          }
+          label="Same for Shipping Address"
+        />
+      </Box>
+    </Box>
+  );
+};
+
+export default Shipping;
