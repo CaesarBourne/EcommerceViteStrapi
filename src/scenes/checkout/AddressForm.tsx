c3bd3f7c171b86c6bb2ef1b5482a07617794fc78
Emmanuel Ajiboye Adeleke <caesar@Emmanuels-MacBook-Pro.local>
2023-10-05 22:23:59 +0100

    adding newcorreced code and text fields

--- src/scenes/checkout/AddressForm.tsx
+++ src/scenes/checkout/AddressForm.tsx
@@ -1,19 +1,101 @@
-import { useSelector } from "react-redux";
-import { Box, Button, Stepper, Step, StepLabel } from "@mui/material";
-import { Formik } from "formik";
-import { useState } from "react";
-import * as yup from "yup";
-import { shades } from "../../theme";
-import Payment from "./Payment";
-import Shipping from "./Shipping";
-import { loadStripe } from "@stripe/stripe-js";
+import { getIn } from "formik";
+import { Box } from "@mui/material";
+import TextField from "@mui/material/TextField";
+import useMediaQuery from "@mui/material/useMediaQuery";
 
-const stripePromise = loadStripe(
-  "pk_test_51LgU7yConHioZHhlAcZdfDAnV9643a7N1CMpxlKtzI1AUWLsRyrord79GYzZQ6m8RzVnVQaHsgbvN1qSpiDegoPi006QkO0Mlc"
-);
+const AddressForm = ({
+  type,
+  values,
+  touched,
+  errors,
+  handleBlur,
+  handleChange,
+}: any) => {
+  const isNonMobile = useMediaQuery("(min-width:600px)");
 
-const AddressForm = () => {
-  return <div>AddressForm</div>;
+  // these functions allow for better code readability
+  const formattedName = (field) => `${type}.${field}`;
+
+  const formattedError = (field) =>
+    Boolean(
+      getIn(touched, formattedName(field)) &&
+        getIn(errors, formattedName(field))
+    );
+
+  const formattedHelper = (field) =>
+    getIn(touched, formattedName(field)) && getIn(errors, formattedName(field));
+
+  return (
+    <Box
+      display="grid"
+      gap="15px"
+      gridTemplateColumns="repeat(4, minmax(0, 1fr))"
+      sx={{
+        "& > div": { gridColumn: isNonMobile ? undefined : "span 4" },
+      }}
+    >
+      <TextField
+        fullWidth
+        type="text"
+        label="First Name"
+        onBlur={handleBlur}
+        onChange={handleChange}
+        value={values.firstName}
+        name={formattedName("firstName")}
+        error={formattedError("firstName")}
+        helperText={formattedHelper("firstName")}
+        sx={{ gridColumn: "span 2" }}
+      />
+      <TextField
+        fullWidth
+        type="text"
+        label="Last Name"
+        onBlur={handleBlur}
+        onChange={handleChange}
+        value={values.lastName}
+        name={formattedName("lastName")}
+        error={formattedError("lastName")}
+        helperText={formattedHelper("lastName")}
+        sx={{ gridColumn: "span 2" }}
+      />
+      <TextField
+        fullWidth
+        type="text"
+        label="Country"
+        onBlur={handleBlur}
+        onChange={handleChange}
+        value={values.country}
+        name={formattedName("country")}
+        error={formattedError("country")}
+        helperText={formattedHelper("country")}
+        sx={{ gridColumn: "span 4" }}
+      />
+      <TextField
+        fullWidth
+        type="text"
+        label="Street Address"
+        onBlur={handleBlur}
+        onChange={handleChange}
+        value={values.street1}
+        name={formattedName("street1")}
+        error={formattedError("street1")}
+        helperText={formattedHelper("street1")}
+        sx={{ gridColumn: "span 2" }}
+      />
+      <TextField
+        fullWidth
+        type="text"
+        label="Street Address 2 (optional)"
+        onBlur={handleBlur}
+        onChange={handleChange}
+        value={values.street2}
+        name={formattedName("street2")}
+        error={formattedError("street2")}
+        helperText={formattedHelper("street2")}
+        sx={{ gridColumn: "span 2" }}
+      />
+    </Box>
+  );
 };
 
 export default AddressForm;
