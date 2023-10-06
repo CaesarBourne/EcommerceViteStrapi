036696872e85bb3e780e7eeea24ee11247938fb4
Emmanuel Ajiboye Adeleke <caesar@Emmanuels-MacBook-Pro.local>
2023-10-06 22:26:52 +0100

    main carousel icon button changes

--- src/scenes/home/MainCarousel.tsx
+++ src/scenes/home/MainCarousel.tsx
@@ -19,7 +19,42 @@ export const heroTextureImports = importAll(
 const MainCarousel = () => {
   const isNonMobile = useMediaQuery("(min-width:600px)");
   return (
-   
+    <Carousel
+      infiniteLoop={true}
+      showThumbs={false}
+      showIndicators={false}
+      showStatus={false}
+      renderArrowPrev={(onClickHandler, hasPrev, label) => (
+        <IconButton
+          onClick={onClickHandler}
+          sx={{
+            position: "absolute",
+            top: "50%",
+            left: "0",
+            color: "white",
+            padding: "5px",
+            zIndex: "10",
+          }}
+        >
+          <NavigateBeforeIcon sx={{ fontSize: 40 }} />
+        </IconButton>
+      )}
+      renderArrowNext={(onClickHandler, hasNext, label) => (
+        <IconButton
+          onClick={onClickHandler}
+          sx={{
+            position: "absolute",
+            top: "50%",
+            right: "0",
+            color: "white",
+            padding: "5px",
+            zIndex: "10",
+          }}
+        >
+          <NavigateNextIcon sx={{ fontSize: 40 }} />
+        </IconButton>
+      )}
+    ></Carousel>
   );
 };
 
