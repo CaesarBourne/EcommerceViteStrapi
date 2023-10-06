42ae9285bc90c1dc4b8d8d4b555d0138cc65097b
Emmanuel Ajiboye Adeleke <caesar@Emmanuels-MacBook-Pro.local>
2023-10-06 22:23:38 +0100

    main carousel changes base

---
+++ src/scenes/home/MainCarousel.tsx
@@ -0,0 +1,26 @@
+import { Box, Typography, IconButton, useMediaQuery } from "@mui/material";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import NavigateBeforeIcon from "@mui/icons-material/NavigateBefore";
+import NavigateNextIcon from "@mui/icons-material/NavigateNext";
+import { shades } from "../../theme";
+
+// imports all images from assets folder
+const importAll = (r) =>
+  r.keys().reduce((acc, item) => {
+    acc[item.replace("./", "")] = r(item);
+    return acc;
+  }, {});
+
+export const heroTextureImports = importAll(
+  require.context("../../assets", false, /\.(png|jpe?g|svg)$/)
+);
+
+const MainCarousel = () => {
+  const isNonMobile = useMediaQuery("(min-width:600px)");
+  return (
+   
+  );
+};
+
+export default MainCarousel;
