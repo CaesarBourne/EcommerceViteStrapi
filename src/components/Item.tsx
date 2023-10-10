ea3dd7098430b5d06b7a4553c6b922d86dc395f0
Emmanuel Ajiboye Adeleke <caesar@Emmanuels-MacBook-Pro.local>
2023-10-10 21:25:10 +0100

    added item imports

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,4 +1,12 @@
 import React from "react";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { IconButton, Box, Typography, useTheme, Button } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
+import { shades } from "../theme";
+import { addToCart } from "../state";
+import { useNavigate } from "react-router-dom";
 
 const Item = () => {
   return <div>Item</div>;
